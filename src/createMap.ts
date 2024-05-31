a93b2eb353e596f885715fc96632cbaa612c7a46
Minseong Kim <minseong@tosel.co.kr>
2024-05-31 11:31:32 +0900

    [Feat] add export for map creator

--- src/createMap.ts
+++ src/createMap.ts
@@ -2,19 +2,14 @@ export function createMapObject<
   U extends keyof T,
   T extends { [index in U]: string | number }
 >(arr: T[], key: U) {
-  const map = arr.reduce((acc, cur) => {
+  return arr.reduce((acc, cur) => {
     acc[cur[key]] = cur;
     return acc;
   }, {} as Record<T[U], T>);
-  return map;
 }
 export function createMap<
   U extends keyof T,
   T extends { [index in U]: string | number }
 >(arr: T[], key: U) {
-  const map = arr.reduce((acc, cur) => {
-    acc.set(cur[key], cur);
-    return acc;
-  }, new Map<T[U], T>());
-  return map;
+  return arr.reduce((acc, cur) => acc.set(cur[key], cur), new Map<T[U], T>());
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import { createMap, createMapObject } from "./createMap";
 import getExpressLoggerRouter from "./expressLogger";
 import fetchUtil from "./fetch";
 import createIsAdminIp from "./isAdminIp";
 import KST from "./kst";
 import unixTimestamp from "./unixTimestamp";
 
-export { fetchUtil, createIsAdminIp, KST, unixTimestamp, getExpressLoggerRouter };
+export {
+  fetchUtil,
+  createIsAdminIp,
+  KST,
+  unixTimestamp,
+  getExpressLoggerRouter,
+  createMapObject,
+  createMap,
+};
