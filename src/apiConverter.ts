cc7d9a9585fea8387748ad3884d076bb4c181a8d
minhyeong <minhyeong.dev@gmail.com>
2024-07-12 02:07:29 +0900

    Revert "[Feat] add options in converter"
    This reverts commit ea364db69da7cd842ea98cd8e6df66ccbcf56a1a.

--- src/apiConverter.ts
+++ src/apiConverter.ts
@@ -29,22 +29,16 @@ export type FromApi<T> = {
  * @returns 변환된 값
  */
 export const toApi: <T extends ObjectInterface, U extends ToApi<T>>(
-  object: T,
-  options?: { changeBoolean?: boolean }
+  object: T
 ) => U = <T extends { createdAt?: Date; updatedAt?: Date }, U extends ToApi<T>>(
-  object: T,
-  options: { changeBoolean?: boolean } = { changeBoolean: false }
+  object: T
 ) => {
   let a = {} as U;
   for (const key in object) {
     const value = object[key];
     if ((key === "createdAt" || key === "updatedAt") && value instanceof Date) {
       a[key] = unixTimestamp.to(value) as U[typeof key];
-    } else if (
-      options.changeBoolean &&
-      /^is[A-Z]/.test(key) &&
-      typeof value === "number"
-    ) {
+    } else if (/^is[A-Z]/.test(key) && typeof value === "number") {
       a[key] = (value === 1 ? true : false) as U[typeof key];
     } else {
       a[key] = value as any;
@@ -54,14 +48,12 @@ export const toApi: <T extends ObjectInterface, U extends ToApi<T>>(
 };
 
 export const fromApi: <T extends ApiInterface, U extends FromApi<T>>(
-  object: T,
-  options?: { changeBoolean?: boolean }
+  object: T
 ) => U = <
   T extends FromApi<U>,
   U extends { createdAt?: number; updatedAt?: number }
 >(
-  object: U,
-  options: { changeBoolean?: boolean } = { changeBoolean: false }
+  object: U
 ): T => {
   const result = {} as T;
 
@@ -72,11 +64,7 @@ export const fromApi: <T extends ApiInterface, U extends FromApi<T>>(
       typeof value === "number"
     ) {
       result[key as keyof T] = unixTimestamp.from(value) as any;
-    } else if (
-      options.changeBoolean &&
-      /^is[A-Z]/.test(key) &&
-      typeof value === "boolean"
-    ) {
+    } else if (/^is[A-Z]/.test(key) && typeof value === "boolean") {
       result[key as keyof T] = (value ? 1 : 0) as any;
     } else {
       result[key as keyof T] = value as any;
