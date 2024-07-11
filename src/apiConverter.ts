4cbafdb3a6a383ed1003b1c45c8cdc18a819ee9e
minhyeong <minhyeong.dev@gmail.com>
2024-07-12 02:11:43 +0900

    [Feat] add expiredAt

--- src/apiConverter.ts
+++ src/apiConverter.ts
@@ -3,21 +3,25 @@ import unixTimestamp from "./unixTimestamp";
 export interface ObjectInterface {
   createdAt?: Date;
   updatedAt?: Date;
+  expiredAt?: Date;
   [key: string]: any;
 }
 
 export interface ApiInterface {
   createdAt?: number;
   updatedAt?: number;
+  expiredAt?: number;
   [key: string]: any;
 }
 
 export type ToApi<T> = {
-  [K in keyof T]: K extends "createdAt" | "updatedAt" ? number : T[K];
+  [K in keyof T]: K extends "createdAt" | "updatedAt" | "expiredAt"
+    ? number
+    : T[K];
 };
 export type FromApi<T> = {
   [K in keyof T]: T[K] extends number
-    ? K extends "createdAt" | "updatedAt"
+    ? K extends "createdAt" | "updatedAt" | "expiredAt"
       ? Date
       : number
     : T[K];
@@ -30,13 +34,19 @@ export type FromApi<T> = {
  */
 export const toApi: <T extends ObjectInterface, U extends ToApi<T>>(
   object: T
-) => U = <T extends { createdAt?: Date; updatedAt?: Date }, U extends ToApi<T>>(
+) => U = <
+  T extends { createdAt?: Date; updatedAt?: Date; expiredAt?: Date },
+  U extends ToApi<T>
+>(
   object: T
 ) => {
   let a = {} as U;
   for (const key in object) {
     const value = object[key];
-    if ((key === "createdAt" || key === "updatedAt") && value instanceof Date) {
+    if (
+      (key === "createdAt" || key === "updatedAt" || key === "expiredAt") &&
+      value instanceof Date
+    ) {
       a[key] = unixTimestamp.to(value) as U[typeof key];
     } else if (/^is[A-Z]/.test(key) && typeof value === "number") {
       a[key] = (value === 1 ? true : false) as U[typeof key];
@@ -51,7 +61,7 @@ export const fromApi: <T extends ApiInterface, U extends FromApi<T>>(
   object: T
 ) => U = <
   T extends FromApi<U>,
-  U extends { createdAt?: number; updatedAt?: number }
+  U extends { createdAt?: number; updatedAt?: number; expiredAt?: number }
 >(
   object: U
 ): T => {
@@ -60,7 +70,7 @@ export const fromApi: <T extends ApiInterface, U extends FromApi<T>>(
   for (const key in object) {
     const value = object[key];
     if (
-      (key === "createdAt" || key === "updatedAt") &&
+      (key === "createdAt" || key === "updatedAt" || key === "expiredAt") &&
       typeof value === "number"
     ) {
       result[key as keyof T] = unixTimestamp.from(value) as any;
