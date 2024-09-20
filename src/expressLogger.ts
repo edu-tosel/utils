48c288e29772a0d3fecb56f483cd10601184048e
Minseong Kim <minseong@tosel.co.kr>
2024-09-20 19:16:06 +0900

    [Feat] reverse dns store

--- src/expressLogger.ts
+++ src/expressLogger.ts
@@ -1,4 +1,16 @@
-export default async function getExpressLoggerRouter() {
+export default async function getExpressLoggerRouter({
+  reverseDnsStore,
+}: {
+  /**
+   * reverseDnsStore is a key-value store that maps IP addresses to domain names.
+   * You can change from IP address to domain name.
+   * @example
+   * {
+   *  "127.0.0.1": "localhost"
+   * }
+   */
+  reverseDnsStore: Record<string, string>;
+}) {
   const { Router } = await import("express");
   const morgan = await import("morgan");
 
@@ -6,6 +18,23 @@ export default async function getExpressLoggerRouter() {
   morgan.token("local-time", (req) => {
     const now = new Date();
     return now.toLocaleString("ko-KR", { timeZone: "Asia/Seoul" });
+  });
+  morgan.token("remote-addr", (req) => {
+    /**
+     *  from morgan source code
+     */
+    function getip(req: any): string | undefined {
+      return (
+        req.ip ||
+        req._remoteAddress ||
+        (req.connection && req.connection.remoteAddress) ||
+        undefined
+      );
+    }
+    const ip = getip(req);
+    if (!ip) return "unknown";
+    else if (reverseDnsStore[ip]) return reverseDnsStore[ip];
+    else return ip;
   });
   logRouter.use(
     morgan.default(
