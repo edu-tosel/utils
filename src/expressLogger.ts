cbe315be189a182b8856d5e1dbc299cfb12c4048
Minseong Kim <minseong@tosel.co.kr>
2024-09-21 14:53:29 +0900

    [Fix] default value

--- src/expressLogger.ts
+++ src/expressLogger.ts
@@ -1,47 +1,44 @@
-export default async function getExpressLoggerRouter(
-  {
-    reverseDnsStore,
-    timeOptions,
-  }: {
-    /**
-     * reverseDnsStore is a key-value store that maps IP addresses to domain names.
-     * You can change from IP address to domain name.
-     * @example
-     * {
-     *  "127.0.0.1": "localhost"
-     * }
-     */
-    reverseDnsStore: Record<string, string>;
-    /**
-     * timeOptions is a option for Intl.DateTimeFormat.
-     * You can change the time format.
-     * @default
-     * {
-     *   timeZone: "Asia/Seoul",
-     *   day: "2-digit",
-     *   month: "2-digit",
-     *   year: undefined,
-     *   hour: "2-digit",
-     *   minute: "2-digit",
-     *   second: "2-digit",
-     * }
-     */
-    timeOptions?: Intl.DateTimeFormatOptions;
-  } = {
-    reverseDnsStore: {},
-    timeOptions: {
-      timeZone: "Asia/Seoul",
-      day: "2-digit",
-      month: "2-digit",
-      year: undefined,
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    },
-  }
-) {
+export default async function getExpressLoggerRouter({
+  reverseDnsStore,
+  timeOptions,
+}: {
+  /**
+   * reverseDnsStore is a key-value store that maps IP addresses to domain names.
+   * You can change from IP address to domain name.
+   * @example
+   * {
+   *  "127.0.0.1": "localhost"
+   * }
+   */
+  reverseDnsStore?: Record<string, string>;
+  /**
+   * timeOptions is a option for Intl.DateTimeFormat.
+   * You can change the time format.
+   * @default
+   * {
+   *   timeZone: "Asia/Seoul",
+   *   day: "2-digit",
+   *   month: "2-digit",
+   *   year: undefined,
+   *   hour: "2-digit",
+   *   minute: "2-digit",
+   *   second: "2-digit",
+   * }
+   */
+  timeOptions?: Intl.DateTimeFormatOptions;
+} = {}) {
   const { Router } = await import("express");
   const morgan = await import("morgan");
+  reverseDnsStore = reverseDnsStore || {};
+  timeOptions = timeOptions || {
+    timeZone: "Asia/Seoul",
+    day: "2-digit",
+    month: "2-digit",
+    year: undefined,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
 
   const logRouter = Router();
   morgan.token("local-time", (req) => {
